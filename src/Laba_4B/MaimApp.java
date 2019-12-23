b5dba2bc8f9b0202af7842275a45802326da2081
h8pool <58972549+h8pool@users.noreply.github.com>
2019-12-23 19:39:28 +0300

    Push

---
+++ src/Laba_4B/MaimApp.java
@@ -0,0 +1,14 @@
+package Laba_4B;
+
+import javax.swing.*;
+
+public class MaimApp {
+    public static void main(String[] args)
+    {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+
+}
